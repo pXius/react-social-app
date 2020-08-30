940362f042fffdef7367fc584c02998ab05ec720
pXius <64472458+pXius@users.noreply.github.com>
2020-08-30 18:14:27 +0200

    WebpackUpdates

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,8 +4,9 @@ import HeaderLoggedOut from "./HeaderLoggedOut";
 import HeaderLoggedIn from "./HeaderLoggedIn";
 import StateContext from "../StateContext";
 
-function Header() {
+function Header(props) {
   const appState = useContext(StateContext);
+  const headerContent = appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />;
 
   return (
     <header className="header-bar bg-primary mb-3">
@@ -15,7 +16,7 @@ function Header() {
             Interact
           </Link>
         </h4>
-        {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+        {!props.staticEmpty && headerContent}
       </div>
     </header>
   );
